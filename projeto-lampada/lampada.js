4188ac21af95e1a26e7d139c6cdcd5c4e7a51e6e
Marcelo Oliveira <marcelovl1970@hotmail.com>
2022-05-25 17:57:45 -0300

    Ajuste de codigo no lampada.js

--- projeto-lampada/lampada.js
+++ projeto-lampada/lampada.js
@@ -2,9 +2,9 @@ let lamp = document.getElementById('imagem');
 lamp.onclick = function() {
    if (lamp.src.match('lampada-on')) {
     lamp.src="lampada-off.gif";
-    alert("Lampada apagada");
+    alert('Lámpada apagada');
    } else {
     lamp.src="lampada-on.gif";
-alert("lampada acesa");
+alert('Lámpada acesa');
 }
 }
