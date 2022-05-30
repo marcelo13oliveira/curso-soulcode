0feef1fe22d5717071bdd8a67aaa7d746f44189c
Marcelo Oliveira <marcelovl1970@hotmail.com>
2022-05-30 08:39:25 -0300

    Atualisado para demonstrar a leitores de telas, quando a lâmpada está acesa ou apagada.

--- projeto-lampada/lampada.js
+++ projeto-lampada/lampada.js
@@ -2,9 +2,10 @@ let lamp = document.getElementById('imagem');
 lamp.onclick = function() {
    if (lamp.src.match('lampada-on')) {
     lamp.src="lampada-off.gif";
-    alert('Lámpada apagada');
+    document.getElementById('trocar').innerHTML = "Lâmpada apagada!";
+    // alert('Lámpada apagada');
    } else {
     lamp.src="lampada-on.gif";
-alert('Lámpada acesa');
+    document.getElementById('trocar').innerHTML = "Lâmpada acesa!";
 }
 }
